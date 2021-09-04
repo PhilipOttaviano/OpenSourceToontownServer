87839d8e8eab3a424985915f6b407759e4eab96a
Dudo360 <87421576+PhilipOttaviano@users.noreply.github.com>
2021-09-04 02:11:08 -0400

    I was trippin xD + sfx folder for phase_14

--- game/TestPython.py
+++
@@ -1,8 +0,0 @@
-from direct.distributed import DistributedObject
-
-class DistributedPythonTest(DistributedObject.DistributedObject):
-  def __init__(self, cr):
-
-      DistributedObject.DistributedObject.__init__(self, cr)
-
-      self.cr = cr
